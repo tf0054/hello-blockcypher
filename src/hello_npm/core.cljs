29ad475a2a2b82f2b86a57db7bb2d52dff38124b
Takeshi Nakano <tf0054@gmail.com>
2016-07-30 00:21:45 +0200

    Added some operations on web3

--- src/hello_npm/core.cljs
+++ src/hello_npm/core.cljs
@@ -8,19 +8,58 @@
 (nodejs/enable-util-print!)
 
 (def ping (nodejs/require "ping"))
-(def web3 (nodejs/require "web3"))
+(def web3obj (nodejs/require "web3"))
+
+(defn sleep [msec]
+  (let [deadline (+ msec (.getTime (js/Date.)))]
+(while (> deadline (.getTime (js/Date.))))))
+
+(defn unlockAccount [web3 addr passwd duration]
+    ;web3.personal.unlockAccount("0x1234...","password")
+    (.unlockAccount (.-personal web3)
+                     addr passwd duration)
+    )
+
+(defn mkTransaction [eth from to amount f]
+    ;web3.eth.sendTransaction(transactionObject [, callback])
+    (let [obj (clj->js {:to to
+                        :from from
+                        :value amount})]
+        (println "obj:" obj)
+        (.sendTransaction eth obj f) ) )
 
 (defn -main [& args]
-    (let [web3conn (web3.)
-          web3prov (web3.providers.HttpProvider. "http://localhost:8545")]
+    (let [web3 (web3obj.)
+          web3prov (web3obj.providers.HttpProvider. "http://localhost:8545")]
         ; init
         ;   web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
-        (.setProvider web3conn web3prov)
+        (.setProvider web3 web3prov)
         ; exec
-        (let [coinbase (.-coinbase (.-eth web3conn))
-              balance (.getBalance (.-eth web3conn) coinbase)]
-            (println "coinbase:" coinbase)
+        (let [eth (.-eth web3)
+              coinbase (.-coinbase eth)
+              balance (.getBalance eth coinbase)
+              accounts (js->clj (.-accounts eth))]
+            (println "coinbase: " coinbase)
             (println "balance: " (.toFormat balance 2))
+            (println "lastblock: " (js->clj (.getBlock eth "latest")))
+            (println "accounts: " accounts)
+
+            ; unlock
+            (println
+              (doall (map #(unlockAccount web3 % "password" 300)
+                          accounts)) )
+            ; transfer
+            (println "transaction: " (mkTransaction eth
+                                                    (nth accounts 0)
+                                                    (nth accounts 1)
+                                                    10
+                                                    #(println "tx-hash:" %) ) )
+            (sleep (* 100 10))
+            ; check balances
+            (println "wakeup")
+              (doall (map #(let [bal (.getBalance eth %)]
+                               (println % "->" (.toFormat bal 2)) )
+                          accounts))
             )
         )
     )
