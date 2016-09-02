31d65a5dd14fe852947357b29efa4acc364882a7
Takeshi Nakano <tf0054@gmail.com>
2016-09-03 01:41:37 +0200

    Adding Profile2.js

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hello-shh "0.1.0-SNAPSHOT"
+(defproject hello-jsx "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.8.0"]
--- src/hello_shh/core.cljs
+++ src/hello_shh/core.cljs
@@ -19,7 +19,10 @@
 (def opn (nodejs/require "opn"))
 (def web3obj (nodejs/require "web3"))
 (def sqlite3 (nodejs/require "sqlite3"))
-(def resume (nodejs/require "/work/tf0054/work/hello-shh/node/applicant/Resumes2.js"))
+
+(def utils-merge (nodejs/require "utils-merge"))
+(def resume (nodejs/require "/work/tf0054/work/hello-jsx/node/applicant/Resumes2.js"))
+(def profile (nodejs/require "/work/tf0054/work/hello-jsx/node/applicant/Profile2.js"))
 
 (defonce db-args (atom {}))
 (defonce db-locl (atom {}))
@@ -35,16 +38,19 @@
             row (js->clj x :keywordize-keys true)]
         (println "Read: " tbl)
         (doall (for [y row]
-                 (swap! db-locl assoc-in [:sys (keyword (:name y))] (:value y)) ))
+                 (swap! db-locl
+                        assoc-in [:sys (keyword (:name y))] (:value y)) ))
         (>! c @db-locl) ) )
   )
 
 (defn -main [& args]
-    ; checking args
+  ; checking args
   (let [x   (args/parseOpts args)
         o   (:options x)
         _db (.-Database sqlite3)
-        db  (_db. "test.db")
+        db  (new _db "test.db")
+        ;; db (new (.-Database sqlite3) "test.db")
+        ;; db (.Database sqlite3 "test.db")
         c   (chan 1)]
         (if (or (not (nil? (x :errors))) (contains? o :help))
             (do (println (:errors x) "\n" "How to use:")
@@ -52,17 +58,6 @@
                 (.exit js/process 1))
             (do (swap! db-args merge o) ) )
 
-        ;; (.serialize db (fn []
-        ;;                  (println "Prepared.")
-        ;;                  (.run db "DROP TABLE IF EXISTS lorem")
-        ;;                  (.run db "CREATE TABLE lorem (info TEXT)")
-        ;;                  (let [stmt (.prepare db "INSERT INTO lorem VALUES (?)")]
-        ;;                    (println "Write:")
-        ;;                    (doall (for [i (range 10)]
-        ;;                             (.run stmt (str "Ipsum" i) #(println "w")) ) )                           
-        ;;                    (.finalize stmt) )
-        ;;                  (go (>! c 1) )
-        ;;                  ))
         (go (>! c 1) )
 
         (go (let [x (<! c)]  
@@ -72,49 +67,49 @@
               (>! c 1) ) )
 
         (mkMap db "sys" c)
-        
+
         (go (let [x (<! c)]
               (println "RES:" x)
-              (.all db
-                    "SELECT rowid AS id, info FROM lorem"
-                    (h/>? c) )
-              ) )
-        
-        (go (let [x (<! c) ; if err occured, num of args would become 2?
-                  row (js->clj x :keywordize-keys true)]
-              (println "Read:")
-              (doall (for [y row]
-                       (println (:id y) ":" (:info y)) ))
-              (>! c 1) ) )
-        
-        (go (let [x (<! c)]
               (println "Close.")
               (.close db)
               (>! c 1)) )
-        ; main
-        (let [web3     (web3obj.)
-              web3prov (web3obj.providers.HttpProvider. (:geth @db-args))]
-          ; init web3
-          (.setProvider web3 web3prov)
-          ; exec
 
-          (go (let [x (<! c)]
-                (let [resumeCljs  (new (.-default resume) (clj->js {:web3 web3}))
-                      prop {:abi (:abi (:sys @db-locl))
-                            :systemAgent (:systemagent (:sys @db-locl))
-                            }
+        ; main
+        (go (let [x (<! c)]
+              (let [web3     (web3obj.)
+                    web3prov (web3obj.providers.HttpProvider. (:geth @db-args))]
+                ; init web3
+                (.setProvider web3 web3prov)
+
+                ; exec
+                (let [resumeCljs (new (.-default resume) (clj->js {:web3 web3}))
+                      profileCljs (new (.-default profile) (clj->js {:web3 web3})) 
+                      prop       {:abi         (:abi (:sys @db-locl))
+                                  :systemAgent (:systemagent (:sys @db-locl))
+                                  }
                       ]
+
+                  ; replace Component's setState bc it checks "mounted" status.
+                  (set! (.-setState profileCljs)
+                        (fn [x]
+                          ;; (println x "," (.-state profileCljs))
+                          (utils-merge (.-state profileCljs) x) ) )
+
+                  (.nameChange profileCljs (clj->js {:target {:value "test3"}}))
+                  (.birthdayChange profileCljs (clj->js {:target {:value "1976/12/06"}}))
                   
+                  (println "props:" (.-props profileCljs))
+                  (println "state:" (.-state profileCljs))
                   (println "god-res:" (.loadOrganizations resumeCljs (clj->js prop)))  
                   )    
-                ) )          
-
-          (let [eth      (.-eth web3)
-                coinbase (.-coinbase eth)
-                ch       (chan)]
-            (println "coinbase:" coinbase))
-          )
+                (let [eth      (.-eth web3)
+                      coinbase (.-coinbase eth)
+                      ch       (chan)]
+                  (println "coinbase:" coinbase))
+                )
+              )
+            )
         )
- )
+  )
 
 (set! *main-cli-fn* -main)
